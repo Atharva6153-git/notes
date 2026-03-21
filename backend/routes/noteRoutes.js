6fd85cc55e639f103a8c138d0a8f3dac353a49c8
Atharva <atharvaj7822@gmail.com>
2026-03-21 22:29:36 +0530

    added edit/update feature

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -36,4 +36,18 @@ router.delete('/:id', async (req, res) => {
   }
 })
 
+// PUT update a note
+router.put('/:id', async (req,res)=> {
+  try{
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      {title: req.body.title, body: req.body.body},
+      {new: true}
+    )
+    res.json(updatedNote)
+  } catch (error) {
+    res.status(500).json({ message: error.message})
+  }
+})
+
 module.exports = router
